c51031dc90d434f9c4d5ce306fb2ec3d38ec2df9
ANU <aniket@sabkuch.com>
2014-01-25 16:00:36 +0530

    sample test added

---
+++ test/aniketsr/lib/SampleTest.java
@@ -0,0 +1,12 @@
+package aniketsr.lib;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SampleTest {
+    @Test
+    public void test_one(){
+        assertEquals(true,true);
+    }
+}
